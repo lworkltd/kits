2d0a8444141d855686b55e352d561d859215cbf5
keto <keto@lwork.com>
2017-07-22 14:07:13 +0800

    update conf

--- example/simple/conf/conf.go
+++ example/simple/conf/conf.go
@@ -1,5 +1,20 @@
 package conf
 
-type Profile struct {
+type Application struct {
 	MongoUrl string `env:"mongo.url" toml:"mongo.url"`
+}
+
+type Mongo struct {
+}
+
+type Redis struct {
+}
+
+type Zipkin struct {
+}
+
+type Discovery struct {
+}
+
+type Consul struct {
 }
