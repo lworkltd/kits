99695a0c72722aa81523428d840f393ac4914204
keto <keto@lwork.com>
2017-07-23 14:56:52 +0800

    add eval

--- pkgs/eval/eval.go
+++ pkgs/eval/eval.go
@@ -1,5 +1,11 @@
 package eval
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
 // Eval 是一个根据表达式获得值的接口
 type Eval interface {
 	Value(string) (string, error)
@@ -12,8 +18,111 @@ func Value(s string) (string, error) {
 	return defaultEval.Value(s)
 }
 
-type EvalString string
+type evalImpl struct {
+}
 
-func (typ EvalString) Eval() (string, error) {
-	return Value(string(typ))
+func (impl evalImpl) Eval(desc string) (str string, err error) {
+	index := strings.Index(desc, "$(")
+	if index < 0 {
+		return desc, nil
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			if s, ok := r.(string); ok {
+				err = fmt.Errorf(s)
+				return
+			}
+			panic(err)
+		}
+	}()
+
+	var buffer bytes.Buffer
+	executeDesc(&buffer, desc[0:])
+
+	return buffer.String(), nil
+}
+
+func executeDesc(buffer *bytes.Buffer, desc string) {
+	index := strings.Index(desc, "$(")
+	if index < 0 {
+		buffer.WriteString(desc)
+		return
+	}
+	if index > 0 {
+		buffer.WriteString(desc[:index])
+		desc = desc[index:]
+	}
+	end := strings.Index(desc, ")")
+	if end < 0 {
+		panic(fmt.Sprintf("bad syntax,expect need comma, from %v", desc))
+	}
+
+	input := desc[2:end]
+	name, args, err := parseDesc(input)
+	if err != nil {
+		panic(err.Error())
+	}
+	execFunc, exist := executors[name]
+	if !exist {
+		panic(fmt.Sprintf("exector %s not found", name))
+	}
+
+	result, _, err := execFunc(args...)
+	if err != nil {
+		panic(err.Error())
+	}
+	buffer.WriteString(result)
+	if len(desc)-1 > end {
+		executeDesc(buffer, desc[end+1:])
+	}
+}
+
+type ExecutorFunc func(...string) (string, bool, error)
+
+var executors map[string]ExecutorFunc
+
+func init() {
+	executors = make(map[string]ExecutorFunc, 10)
+}
+
+func RegisterExecutor(name string, executor ExecutorFunc) {
+	executors[name] = executor
+}
+
+func RegisterKeyValueExecutor(name string, f func(string) (string, bool, error)) {
+	executors[name] = SingleArgsExecutor(f)
+}
+
+type executor struct {
+	exec ExecutorFunc
+	args []string
+}
+
+func parseDesc(desc string) (string, []string, error) {
+	desc = strings.TrimLeft(desc, " ")
+	if desc == "" {
+		return "", nil, fmt.Errorf("bad eval syntax")
+	}
+	words := strings.Split(desc, " ")
+	var args []string
+	if len(words) > 1 {
+		args = words[1:]
+	}
+	return words[0], args, nil
+}
+
+func EmptyArgsExecutor(f func() (string, bool, error)) ExecutorFunc {
+	return func(...string) (string, bool, error) {
+		return f()
+	}
+}
+
+func SingleArgsExecutor(f func(string) (string, bool, error)) ExecutorFunc {
+	return func(args ...string) (string, bool, error) {
+		if len(args) < 1 {
+			return "", false, fmt.Errorf("at least one args")
+		}
+		return f(args[0])
+	}
 }
---
+++ pkgs/eval/eval_test.go
@@ -0,0 +1,240 @@
+package eval
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseDesc(t *testing.T) {
+	type args struct {
+		desc string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   []string
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				desc: "ip_of_interface x y z ",
+			},
+			want:    "ip_of_interface",
+			want1:   []string{"x", "y", "z", ""},
+			wantErr: false,
+		},
+		{
+			name: "noargs",
+			args: args{
+				desc: "ip_of_firt_interface",
+			},
+			want:    "ip_of_firt_interface",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "space noargs",
+			args: args{
+				desc: "  ip_of_firt_interface",
+			},
+			want:    "ip_of_firt_interface",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "error",
+			args: args{
+				desc: "",
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := parseDesc(tt.args.desc)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseDesc() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("parseDesc() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("parseDesc() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestSingleArgsExecutor(t *testing.T) {
+	type args struct {
+		key []string
+		f   func(string) (string, bool, error)
+	}
+	tests := []struct {
+		name    string
+		value   string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				key: []string{"key"},
+				f: func(string) (string, bool, error) {
+					return "value", true, nil
+				},
+			},
+			value:   "value",
+			wantErr: false,
+		},
+		{
+			name: "lack key",
+			args: args{
+				key: []string{},
+				f: func(string) (string, bool, error) {
+					return "value", true, nil
+				},
+			},
+			value:   "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _, err := SingleArgsExecutor(tt.args.f)(tt.args.key...)
+			if tt.wantErr && err == nil {
+				t.Errorf("SingleArgsExecutor()  want %v err=%v", tt.wantErr, err)
+			}
+			if !reflect.DeepEqual(got, tt.value) {
+				t.Errorf("SingleArgsExecutor() = %v,  want=%v", got, tt.value)
+			}
+		})
+	}
+}
+
+func TestEmptyArgsExecutor(t *testing.T) {
+	type args struct {
+		f func() (string, bool, error)
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				f: func() (string, bool, error) {
+					return "value", true, nil
+				},
+			},
+			want:    "value",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _, err := EmptyArgsExecutor(tt.args.f)()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseDesc() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EmptyArgsExecutor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_evalImpl_Eval(t *testing.T) {
+	RegisterExecutor("ip_of_interface", SingleArgsExecutor(func(interfaceName string) (string, bool, error) {
+		if interfaceName == "eth0" {
+			return "127.0.0.1", false, nil
+		}
+		return "", false, nil
+	}))
+
+	RegisterKeyValueExecutor("kv_of_consul", func(key string) (string, bool, error) {
+		if key == "key" {
+			return "value", true, nil
+		}
+		return "", false, nil
+	})
+	type args struct {
+		desc string
+	}
+	tests := []struct {
+		name    string
+		impl    evalImpl
+		args    args
+		wantStr string
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				desc: "$(ip_of_interface eth0):8080",
+			},
+			wantStr: "127.0.0.1:8080",
+			wantErr: false,
+		},
+		{
+			name: "prefix",
+			args: args{
+				desc: "http://$(ip_of_interface eth0):8080$%@*@_)(8979723$(kv_of_consul key)",
+			},
+			wantStr: "http://127.0.0.1:8080$%@*@_)(8979723value",
+			wantErr: false,
+		},
+		{
+			name: "none",
+			args: args{
+				desc: "1233ou21312|{{_*~!",
+			},
+			wantStr: "1233ou21312|{{_*~!",
+			wantErr: false,
+		},
+		{
+			name: "executor_not_found",
+			args: args{
+				desc: "$(executor_not_found 123 123)",
+			},
+			wantStr: "",
+			wantErr: true,
+		},
+		{
+			name: "bad_syntax",
+			args: args{
+				desc: "$(bad_syntax ",
+			},
+			wantStr: "",
+			wantErr: true,
+		},
+		{
+			name: "bad_syntax2",
+			args: args{
+				desc: "$()",
+			},
+			wantStr: "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotStr, err := tt.impl.Eval(tt.args.desc)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("evalImpl.Eval() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotStr != tt.wantStr {
+				t.Errorf("evalImpl.Eval() = %v, want %v", gotStr, tt.wantStr)
+			}
+		})
+	}
+}
