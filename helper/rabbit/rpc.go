8ee3dc14d1488cf28667026ca6d871bf2d940229
keto <keto@lwork.com>
2017-07-28 15:19:28 +0800

    update for adapting the session change

--- helper/rabbit/rpc.go
+++ helper/rabbit/rpc.go
@@ -15,17 +15,18 @@ import (
 	"github.com/streadway/amqp"
 )
 
-type RPCUtil struct {
-	*Session
-	records      map[string]chan *amqp.Delivery
-	recordsMutex sync.Mutex
-	queueName    string
-	timeout      time.Duration
+type RpcUtil struct {
+	sess *Session
+	sync.Mutex
+
+	records map[string]chan *amqp.Delivery
+	replyTo string
+	timeout time.Duration
 }
 
-func NewRPCUtil(sess *Session, timeout time.Duration) *RPCUtil {
-	util := &RPCUtil{
-		Session: sess,
+func NewRpcUtil(sess *Session, timeout time.Duration) *RpcUtil {
+	util := &RpcUtil{
+		sess:    sess,
 		records: make(map[string]chan *amqp.Delivery),
 		timeout: timeout,
 	}
@@ -33,11 +34,11 @@ func NewRPCUtil(sess *Session, timeout time.Duration) *RPCUtil {
 	return util
 }
 
-func (util *RPCUtil) SetupReplyQueue(name string) error {
+func (util *RpcUtil) SetupReplyQueue(name string) error {
 	handler := func(dli *amqp.Delivery) {
-		util.recordsMutex.Lock()
+		util.Lock()
 		record, ok := util.records[dli.CorrelationId]
-		util.recordsMutex.Unlock()
+		util.Unlock()
 
 		if !ok {
 			return
@@ -46,72 +47,51 @@ func (util *RPCUtil) SetupReplyQueue(name string) error {
 		record <- dli
 		close(record)
 
-		util.recordsMutex.Lock()
+		util.Lock()
 		delete(util.records, dli.CorrelationId)
-		util.recordsMutex.Unlock()
+		util.Unlock()
 	}
 
-	queue, err := util.DeclareAndHandleQueue(name, handler, map[string]bool{
-		"queue/durable":    false,
-		"queue/autodelete": true,
-		"queue/exclusive":  false,
-	})
+	queue, err := util.sess.DeclareAndHandleQueue(name, handler,
+		NewQueueSettings().AutoDelete(true).Durable(false).Exclusive(false))
 	if err != nil {
 		return err
 	}
 
-	util.queueName = queue.Name
+	util.replyTo = queue.Name
 
 	return err
 }
 
-// readCorrelationId allow user to pass a `cid` or `correlation_id` or use a generated uuid
-func readCorrelationId(args map[string]string) string {
-	if args == nil {
-		return uuid.New().String()
-	}
-
-	cid, exist := args["cid"]
-	if exist {
-		return cid
-	}
-	cid, exist = args["correlation_id"]
-	if exist {
-		return cid
-	}
-
-	return uuid.New().String()
-}
-
-func (util *RPCUtil) PublishBytes(b []byte, ex, queueOrKey string, args map[string]string) (*amqp.Delivery, error) {
+func (util *RpcUtil) Call(b []byte, ex, queueOrKey string, options ...PublishOption) (*amqp.Delivery, error) {
 	if queueOrKey == "" {
 		return nil, fmt.Errorf("amqp rpc must specify a routing key or queue name")
 	}
 
-	if util.queueName == "" {
+	if util.replyTo == "" {
 		return nil, fmt.Errorf("amqp rpc need a reply queue")
 	}
 
-	cid := readCorrelationId(args)
-	args["cid"] = readCorrelationId(args)
-	args["reply_to"] = util.queueName
-	args["expiration"] = strconv.Itoa(int(util.timeout / time.Millisecond))
-	err := util.Session.PublishBytes(b, ex, queueOrKey, args)
+	cid := uuid.New().String()
+	options = append(options, OptionReplyTo(util.replyTo))
+	options = append(options, OptionCorrelationId(cid))
+	options = append(options, OptionExpiration(strconv.Itoa(int(util.timeout/time.Millisecond))))
+
+	err := util.sess.Publish(b, ex, queueOrKey, options...)
 	if err != nil {
 		return nil, err
 	}
 
 	waitChan := make(chan *amqp.Delivery, 1)
-	util.recordsMutex.Lock()
+	util.Lock()
 	util.records[cid] = waitChan
-	util.recordsMutex.Unlock()
+	util.Unlock()
 
 	select {
 	case <-time.After(util.timeout):
-		util.recordsMutex.Lock()
+		util.Lock()
 		util.records[cid] = waitChan
-		util.recordsMutex.Unlock()
-
+		util.Unlock()
 		return nil, errors.New("amqp rpc time out")
 	case delivery := <-waitChan:
 		return delivery, nil
