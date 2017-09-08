12e6c25f66033a2e4dd91274811efccdffbcc833
steven <steven@lwork.com>
2017-09-08 10:43:42 +0800

    HTTP请求增加byte消息体接口

--- service/invoke/export.go
+++ service/invoke/export.go
@@ -47,6 +47,7 @@ type (
 		Route(string, string) Client         // 添加路径参数
 		Routes(map[string]string) Client     // 添加路径参数
 		Json(interface{}) Client             // 添加Json消息体
+		Body(byte []byte) Client             // 添加byte消息体
 		Context(context.Context) Client      // 上下文
 		Fallback(func(error) error) Client   // 失败触发器
 		Exec(interface{}) (int, error)       // 执行请求
