65a0575a394ddf18ec11d8779d7d427f11c48abc
keto <keto@lwork.com>
2017-09-22 17:51:32 +0800

    add test

--- utils/discovery/discovery_test.go
+++ utils/discovery/discovery_test.go
@@ -1,7 +1,19 @@
 package discovery
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/lworkltd/kits/helper/consul"
+	"github.com/lworkltd/kits/service/discovery"
+	"github.com/lworkltd/kits/service/profile"
+)
+
+func init() {
+	discovery.Init(&discovery.Option{
+		SearchFunc:   func(string) ([]string, []string, error) { return nil, nil, nil },
+		RegisterFunc: func(*consul.RegisterOption) error { return nil },
+	})
+}
 func TestMakeCheckUrl(t *testing.T) {
 	type args struct {
 		ip   string
@@ -37,6 +49,185 @@ func TestMakeCheckUrl(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := makeCheckUrl(tt.args.ip, tt.args.port, tt.args.path); got != tt.want {
 				t.Errorf("makeCheckUrl() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckAndResolveProfile(t *testing.T) {
+	type args struct {
+		cfg *profile.Service
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				cfg: &profile.Service{
+					Host:       ":8080",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "my-service-1",
+				},
+			},
+			want:    8080,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := checkAndResolveProfile(tt.args.cfg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("checkAndResolveProfile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("checkAndResolveProfile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRegisterServerWithProfile(t *testing.T) {
+	type args struct {
+		checkUrl string
+		cfg      *profile.Service
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "normal",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":8080",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "my-service-1",
+				},
+				checkUrl: "/health",
+			},
+			wantErr: false,
+		},
+		{
+			name: "no-register",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "lack-port",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       "",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "my-service-1",
+				},
+				checkUrl: "/health",
+			},
+			wantErr: true,
+		},
+		{
+			name: "lack-ip",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":8080",
+					ReportIp:   "",
+					ReportName: "my-service",
+					ReportId:   "my-service-1",
+				},
+				checkUrl: "/health",
+			},
+			wantErr: true,
+		},
+		{
+			name: "lack-service",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":8080",
+					ReportIp:   "192.168.0.1",
+					ReportName: "",
+					ReportId:   "my-service-1",
+				},
+				checkUrl: "/health",
+			},
+			wantErr: true,
+		},
+		{
+			name: "lack-service-id",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":8080",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "",
+				},
+				checkUrl: "/health",
+			},
+			wantErr: true,
+		},
+		{
+			name: "lack-check-url",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":8080",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "",
+				},
+				checkUrl: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "port-no-number",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":abc",
+					ReportIp:   "192.168.0.1",
+					ReportName: "my-service",
+					ReportId:   "",
+				},
+				checkUrl: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "loopback-forbiden",
+			args: args{
+				cfg: &profile.Service{
+					Reportable: true,
+					Host:       ":abc",
+					ReportIp:   "localhost",
+					ReportName: "my-service",
+					ReportId:   "",
+				},
+				checkUrl: "",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := RegisterServerWithProfile(tt.args.checkUrl, tt.args.cfg); (err != nil) != tt.wantErr {
+				t.Errorf("RegisterServerWithProfile() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
