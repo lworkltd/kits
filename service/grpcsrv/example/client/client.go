0691da5ca297b720d750199e8781fba6e688bb5c
keto <keto@lwork.com>
2018-04-04 17:46:45 +0800

    add grpcinvoke implement and grpc service implement

---
+++ service/grpcsrv/example/client/client.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/Sirupsen/logrus"
+
+	"github.com/lworkltd/kits/service/grpcinvoke"
+	_ "github.com/lworkltd/kits/service/grpcinvoke/invokeimpl"
+	"github.com/lworkltd/kits/service/grpcsrv/example/testproto"
+	"github.com/lworkltd/kits/service/grpcsrv/grpccomm"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableColors: true,
+	})
+	var (
+		err error
+	)
+	req1 := &testproto.DepositRequest{}
+
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("").Header(&testproto.AccountHeader{
+		Account:  "abc",
+		Password: "123",
+	}).Body(req1).Response(nil)
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	req2 := &testproto.CalculateStrLenRequest{}
+
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("").Header(&grpccomm.CommHeader{}).Body(req2).Response(nil)
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	req3 := &testproto.AddRequest{
+		A: 1,
+		B: -2,
+	}
+	rsp3 := &testproto.AddResponse{}
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("").Header(&grpccomm.CommHeader{}).Body(req3).Response(rsp3)
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	fmt.Println("AddResponse", rsp3.Sum)
+
+	req4 := &testproto.AddRequest{
+		A: 1,
+		B: -2,
+	}
+	rsp4 := &testproto.AddResponse{}
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("Agent").Header(&grpccomm.CommHeader{}).Body(req4).Response(rsp4)
+	if err != nil {
+		fmt.Println("Error Agent", err)
+		return
+	}
+	fmt.Println("AddResponse", rsp4.Sum)
+
+	rsp5 := &testproto.DeleteUserRequest{}
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("").Header(&grpccomm.CommHeader{
+		BaseInfo: &grpccomm.BaseInfo{
+			TenantId:  "T001234",
+			XApiToken: "abc",
+		},
+	}).Body(rsp5).Response(nil)
+
+	if err != nil {
+		fmt.Println("Error DeleteUserRequest", err)
+		return
+	}
+
+	rsp6 := &testproto.ReportRequest{}
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("").Header(&grpccomm.CommHeader{}).Body(rsp6).Response(nil)
+	if err != nil {
+		fmt.Println("Error ReportRequest", err)
+		return
+	}
+
+	rsp7 := &testproto.ReportRequest{}
+	err = grpcinvoke.Addr("127.0.0.1:8090").Grpc("Agent").Header(&grpccomm.CommHeader{}).Body(rsp7).Response(nil)
+	if err != nil {
+		fmt.Println("Error Agent", err)
+		return
+	}
+
+}
---
+++ service/grpcsrv/hooks_test.go
@@ -0,0 +1,162 @@
+package grpcsrv
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/lworkltd/kits/service/grpcsrv/grpccomm"
+	"github.com/lworkltd/kits/service/restful/code"
+)
+
+func TestHookRecover(t *testing.T) {
+	type args struct {
+		f HandlerFunc
+	}
+
+	mcodePrefix = "ERROR_"
+	tests := []struct {
+		name      string
+		args      args
+		wantMcode string
+	}{
+		{
+			name: "unkown panic",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					panic("unkown error")
+					return nil
+				},
+			},
+			wantMcode: "GRPC_INTERNAL_ERROR",
+		},
+		{
+			name: "code panic",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					panic(code.New(100, "code error"))
+					return nil
+				},
+			},
+			wantMcode: "ERROR_100",
+		},
+
+		{
+			name: "mcode panic",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					panic(code.NewMcode("NOT_FOUND", "mcode error"))
+					return nil
+				},
+			},
+			wantMcode: "NOT_FOUND",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			newf := HookRecover(tt.args.f)
+			rsp := newf(context.Background(), &grpccomm.CommRequest{})
+			if rsp.Result {
+				t.Errorf("HookRecover want rsp.Result=false,got true")
+				return
+			}
+
+			if rsp.Mcode != tt.wantMcode {
+				t.Errorf("HookRecover want rsp.Mcode=%s,got %s", tt.wantMcode, rsp.Mcode)
+				return
+			}
+		})
+	}
+}
+
+func TestHookLogger(t *testing.T) {
+	type args struct {
+		f HandlerFunc
+	}
+	tests := []struct {
+		name    string
+		args    args
+		perpare func()
+	}{
+		{
+
+			name: "normal ok",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					return &grpccomm.CommResponse{
+						Result: true,
+					}
+				},
+			},
+			perpare: func() {
+				MinWarningDelay = time.Second * 10
+			},
+		},
+		{
+
+			name: "error",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					return &grpccomm.CommResponse{
+						Result: false,
+						Mcode:  "ERROR_1",
+					}
+				},
+			},
+			perpare: func() {
+				MinWarningDelay = time.Second * 10
+			},
+		},
+		{
+
+			name: "latency warning",
+			args: args{
+				f: func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+					time.Sleep(time.Millisecond * 100)
+					return nil
+				},
+			},
+			perpare: func() {
+				MinWarningDelay = time.Millisecond * 100
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.perpare()
+			gotF := HookLogger(tt.args.f)
+			gotF(context.Background(), &grpccomm.CommRequest{
+				ReqInterface: "TestRequest",
+				ReqSercice:   "TestService",
+			})
+		})
+	}
+}
+
+func TestDefenceSlowSideHook(t *testing.T) {
+	type args struct {
+		n int32
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+
+		{
+
+			name: "latency warning",
+			args: args{
+				n: 100,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := DefenceSlowSideHook(tt.args.n)
+			f := got(func(ctx context.Context, commReq *grpccomm.CommRequest) (commRsp *grpccomm.CommResponse) {
+				return nil
+			})
+			f(context.Background(), &grpccomm.CommRequest{})
+		})
+	}
+}
