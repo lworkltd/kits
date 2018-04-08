64c445f3c4179485a17ea8bdafae00deaecf154b
keto <keto@lwork.com>
2018-04-08 13:03:31 +0800

    change the interface of getting grpc client from Grpc(string) to Unary(...string)

--- service/grpcinvoke/invokeimpl/grpc_service.go
+++ service/grpcinvoke/invokeimpl/grpc_service.go
@@ -113,7 +113,7 @@ type grpcService struct {
 }
 
 // Grpc 返回调用实例
-func (grpcService *grpcService) Grpc(callName string) grpcinvoke.Client {
+func (grpcService *grpcService) Unary(args ...string) grpcinvoke.Client {
 	grpcService.mutex.Lock()
 	if grpcService.done {
 		grpcService.mutex.Unlock()
@@ -126,6 +126,10 @@ func (grpcService *grpcService) Grpc(callName string) grpcinvoke.Client {
 		return newErrorGrpcClient(err)
 	}
 
+	var callName string
+	if len(args) > 0 {
+		callName = args[0]
+	}
 	return grpcService.newGrpcClient(callName, conn)
 }
 
